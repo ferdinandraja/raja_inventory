d58a4d0a77a5f38d8aceab80d10c9f5bf1134c22
ferdinandraja <ferdinand.raja@cs.ui.ac.id>
2023-09-21 13:34:47 +0700

    temporary assignment 4

--- main/models.py
+++ main/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     price = models.IntegerField()
